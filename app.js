b65bbdd5f92249e81103d98fa81f6c570098b5c2
Mayank <mayanksharmakv@gmail.com>
2023-08-30 17:46:42 +0530

    deploy on vercel

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 import "dotenv/config";
 import express from "express";
+import path from "path";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 let newTasks = [];
 
 // Middlewares
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 app.use(express.urlencoded({ extended: true }));
-
-app.set("view engine", "ejs");
 
 // routes
 app.get("/", (req, res) => {
